2216dfcd9d82526a5a0d7b864b86950df2551fd0
pjleb90 <pjleb90@gmail.com>
2025-01-19 14:25:49 -0500

    added assets and header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import BookList from "@/components/BookList";
+
+const Home = () => (
+  <>
+    <BookList />
+  </>
+);
+
+
+export default Home;
---
+++ components/BookList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BookList = () => {
+  return (
+    <div>
+      <h2 className='font-bebas-neue text-4xl text-light-100'>Popular Books</h2>
+    </div>
+  )
+}
+
+export default BookList
