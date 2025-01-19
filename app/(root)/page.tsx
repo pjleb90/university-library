04409dac8378f00b69d44ff75308ddb14fa0cb9d
pjleb90 <pjleb90@gmail.com>
2025-01-19 15:57:57 -0500

    added hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
+import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 const Home = () => (
   <>
-    <BookList />
+    <BookOverview {...sampleBooks[0]} />
+
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
   </>
 );
 
